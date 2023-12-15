dcccaa7bad8163b07fbdf5f9c6e9621469aafce3
javier <javier@formatinternet.com>
2023-12-15 12:26:44 +0100

    adding ping

--- src/main/java/example/InfluxDB2Example.java
+++ src/main/java/example/InfluxDB2Example.java
@@ -16,7 +16,7 @@ import com.influxdb.query.FluxTable;
 
 public class InfluxDB2Example {
 
-    private static char[] token = "v0H5iXar0vVpLqCpHCKZiSL00eb5dkr0J8GcLxWIQ0uFdAAWe2gQ5vEwfKONtSLM3_bR9Ua9rGewRcqRLdIseA==".toCharArray();
+    private static char[] token = "lE_GhvB4QzZyw5vI0T7hY5Iu5DSxdtl1j2KTh7hPFLTAkOaqymuDxpUCfHDqb5ZeFYd-qKuUF3Crr5_wwLAI0Q==".toCharArray();
     private static String org = "influx";
     private static String bucket = "influx";
 
@@ -24,41 +24,45 @@ public class InfluxDB2Example {
 
         InfluxDBClient influxDBClient = InfluxDBClientFactory.create("http://localhost:8086", token, org, bucket);
 
-        //
-        // Write data
-        //
-        WriteApiBlocking writeApi = influxDBClient.getWriteApiBlocking();
+	if  (influxDBClient.ping()) {
 
-        //
-        // Write by LineProtocol
-        //
-        writeApi.writeRecord(WritePrecision.NS, "temperature,location=north value=60.0");
+        	//
+        	// Write data
+        	//
+        	WriteApiBlocking writeApi = influxDBClient.getWriteApiBlocking();
 
-        //
-        // Write by Data Point
-        //
-        Point point = Point.measurement("temperature")
-                .addTag("location", "west")
-                .addField("value", 55D)
-                .time(Instant.now().toEpochMilli(), WritePrecision.MS);
+        	//
+        	// Write by LineProtocol
+        	//
+        	writeApi.writeRecord(WritePrecision.NS, "temperature,location=north value=60.0");
 
-        writeApi.writePoint(point);
+        	//
+        	// Write by Data Point
+        	//
+        	Point point = Point.measurement("temperature")
+                	.addTag("location", "west")
+                	.addField("value", 55D)
+                	.time(Instant.now().toEpochMilli(), WritePrecision.MS);
+
+        	writeApi.writePoint(point);
 
 
 
-        //
-        // Write by POJO
-        //
-        Temperature temperature = new Temperature();
-        temperature.location = "south";
-        temperature.value = 62D;
-        temperature.time = Instant.now();
+        	//
+        	// Write by POJO
+        	//
+        	Temperature temperature = new Temperature();
+        	temperature.location = "south";
+        	temperature.value = 62D;
+        	temperature.time = Instant.now();
 
-        writeApi.writeMeasurement( WritePrecision.NS, temperature);
+	        writeApi.writeMeasurement( WritePrecision.NS, temperature);
 
+        }
 
 
         influxDBClient.close();
+	System.exit(0);
     }
 
     @Measurement(name = "temperature")
